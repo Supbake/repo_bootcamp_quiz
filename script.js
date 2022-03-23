b3ad964604f08308cfe8021330b5bf091d5a2ffa
jacob ramirez <jacobnramirez@gmail.com>
2022-03-22 22:15:53 -0700

    added my functions of adding and removing my questions from showing, also added my start function

--- script.js
+++ script.js
@@ -1,24 +1,101 @@
+let currentScore = 0
+let totalScore = 5
 
+function myFunction() {
+    const startButton = document.querySelector("body > section.start-window > button");
+    $(startButton).hide();
 
-function showQuestion(q) {
-    let titleCard = document.getElementById("title");
-    titleCard.textContent = q.title;
-    let alts = document.querySelectorAll('.alts');
-    alts.forEach(function(element,index){
-        element.textContent = q.alts[index];
-    })
+    questionOne();
+
+    setTimeout(timesUp, 1000);
+    
 }
 
-showQuestion(question);
+function questionOne() {
+    const questionOne =document.querySelector("#question-one");
+    $(questionOne).show();
 
-let btn = document.getElementById('btn');
-btn.addEventListener('click', function(){
-});
+    questionOne.addEventListener("click", function(event) {
+        
+        if(event.target.id === "correct-answer-1") {
+            ///do something
+            currentScore = currentScore + 1
+            localStorage.setItem("current score", + currentScore);
+            document.querySelector("#current-score").textContent(currentScore);
+            console.log("correct answer fired");
+        } else {
+            //take away 10 seconds on timer
+            console.log("false answer fired")
+        } 
+        $(questionOne).hide();
+        questionTwo();
+    })
+
+}
+function questionTwo() {
+    const questionTwo =document.querySelector("#question-two");
+    $(questionTwo).show();
+
+    questionTwo.addEventListener("click", function(event) {
+        
+        if(event.target.id === "correct-answer-2") {
+            ///do something
+            currentScore = currentScore + 1
+            localStorage.setItem("current score", + currentScore)
+            console.log("correct answer fired");
+        } else {
+            //take away 10 seconds on timer
+            console.log("false answer fired")
+        } 
+        $(questionTwo).hide();
+    })
+
+}
+
+
+
+
+function timesUp() {
+    alert("Times Up!");
+}
+
+// $("#question-one").show(); 
 
 
 
 
 
+
+
+
+// $("card").hide();
+
+// let countDownDate = new Date("00:60:60").getTime();
+// let timerFun = setInterval(function() {
+//     let now = new Date().getTime();
+//     let distance = countDownDate - now;
+//     let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+//     document.getElementById('timer-element').innerHTML = seconds + " Seconds Left";
+//     if (distance < 0) {
+//         clearInterval(timerFun);
+//         document.getElementById('timer-element').innerHTML = "Times Up!";
+//     }
+// },1000);
+
+// function showQuestion(q) {
+//     let titleCard = document.getElementById("title");
+//     titleCard.textContent = q.title;
+//     let alts = document.querySelectorAll('.alts');
+//     alts.forEach(function(element,index){
+//         element.textContent = q.alts[index];
+//     })
+// }
+
+// showQuestion(question);
+
+// let btn = document.getElementById('btn');
+// btn.addEventListener('click', function(){
+// });
 
 // let questionOne = {
 //     title: "Which is not a JavaScript data type?",
