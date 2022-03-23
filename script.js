3ed45fc5a82ceb782deafe2b3d4a007b6c036a8e
jacob ramirez <jacobnramirez@gmail.com>
2022-03-22 23:25:24 -0700

    finalized my final score, loading and results functions and completed the quiz.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let currentScore = 0
 let totalScore = 5
+let currentUser;
 
 function myFunction() {
     const startButton = document.querySelector("body > section.start-window > button");
@@ -7,7 +8,7 @@ function myFunction() {
 
     questionOne();
 
-    setTimeout(timesUp, 1000);
+    setTimeout(finalScore, 10000);
     
 }
 
@@ -21,7 +22,7 @@ function questionOne() {
             ///do something
             currentScore = currentScore + 1
             localStorage.setItem("current score", + currentScore);
-            document.querySelector("#current-score").textContent(currentScore);
+            document.getElementById("current-score").textContent = currentScore;
             console.log("correct answer fired");
         } else {
             //take away 10 seconds on timer
@@ -32,6 +33,7 @@ function questionOne() {
     })
 
 }
+
 function questionTwo() {
     const questionTwo =document.querySelector("#question-two");
     $(questionTwo).show();
@@ -42,87 +44,108 @@ function questionTwo() {
             ///do something
             currentScore = currentScore + 1
             localStorage.setItem("current score", + currentScore)
+            document.getElementById("current-score").textContent = currentScore;
             console.log("correct answer fired");
         } else {
             //take away 10 seconds on timer
             console.log("false answer fired")
         } 
         $(questionTwo).hide();
+        questionThree();
     })
 
 }
 
+function questionThree() {
+    const questionThree =document.querySelector("#question-three");
+    $(questionThree).show();
 
+    questionThree.addEventListener("click", function(event) {
+        
+        if(event.target.id === "correct-answer-3") {
+            ///do something
+            currentScore = currentScore + 1
+            localStorage.setItem("current score", + currentScore);
+            document.getElementById("current-score").textContent = currentScore;
+            console.log("correct answer fired");
+        } else {
+            //take away 10 seconds on timer
+            console.log("false answer fired")
+        } 
+        $(questionThree).hide();
+        questionFour();
+    })
 
-
-function timesUp() {
-    alert("Times Up!");
 }
 
-// $("#question-one").show(); 
+function questionFour() {
+    const questionFour =document.querySelector("#question-four");
+    $(questionFour).show();
 
+    questionFour.addEventListener("click", function(event) {
+        
+        if(event.target.id === "correct-answer-4") {
+            ///do something
+            currentScore = currentScore + 1
+            localStorage.setItem("current score", + currentScore);
+            document.getElementById("current-score").textContent = currentScore;
+            console.log("correct answer fired");
+        } else {
+            //take away 10 seconds on timer
+            console.log("false answer fired")
+        } 
+        $(questionFour).hide();
+        questionFive();
+    })
 
+}
 
+function questionFive() {
+    const questionFive =document.querySelector("#question-five");
+    $(questionFive).show();
 
+    questionFive.addEventListener("click", function(event) {
+        
+        if(event.target.id === "correct-answer-5") {
+            ///do something
+            currentScore = currentScore + 1
+            localStorage.setItem("current score", + currentScore);
+            document.getElementById("current-score").textContent = currentScore;
+            console.log("correct answer fired");
+        } else {
+            //take away 10 seconds on timer
+            console.log("false answer fired")
+        } 
+        $(questionFive).hide();
+        loading();
+    })
 
+}
 
+function loading() {
+    const loading =document.querySelector("#loading");
+    $(loading).show();
+}
 
+function finalScore() {
+    const loading =document.querySelector("#loading");
+    if (loading) {
+        $(loading).hide();
 
-// $("card").hide();
+    }
+    alert("Pencils Down!");
+    let initals = prompt("Save your initals");
+    currentUser = initals;
+    localStorage.setItem(initals, currentScore)
+    results();
 
-// let countDownDate = new Date("00:60:60").getTime();
-// let timerFun = setInterval(function() {
-//     let now = new Date().getTime();
-//     let distance = countDownDate - now;
-//     let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-//     document.getElementById('timer-element').innerHTML = seconds + " Seconds Left";
-//     if (distance < 0) {
-//         clearInterval(timerFun);
-//         document.getElementById('timer-element').innerHTML = "Times Up!";
-//     }
-// },1000);
+}
 
-// function showQuestion(q) {
-//     let titleCard = document.getElementById("title");
-//     titleCard.textContent = q.title;
-//     let alts = document.querySelectorAll('.alts');
-//     alts.forEach(function(element,index){
-//         element.textContent = q.alts[index];
-//     })
-// }
+function results() {
+    const results =document.querySelector("#results");
+    $(results).show();
+    const resultsstring = document.querySelector("#results-string");
+    resultsstring.innerText = `User: ${currentUser}, Score: ${localStorage.getItem(currentUser)}`
+    $(document.querySelector(".score")).hide();
 
-// showQuestion(question);
-
-// let btn = document.getElementById('btn');
-// btn.addEventListener('click', function(){
-// });
-
-// let questionOne = {
-//     title: "Which is not a JavaScript data type?",
-//     alts: ['Number', 'String', 'Boolean', 'Defined'],
-//     correctAnswer: 3,
-// }
-
-// let questionTwo = {
-//     question: "Which language runs in a web browser",
-//     answers: ['Python', 'JavaScript', 'Cobra', 'Java'],
-//     correctAnswer: 1,
-// }
-
-// let questionThree = {
-//     question: "What does CSS stand for?",
-//     answers: ['Central Style Sheets', 'Common Style Sheets', 'Cascading Style Sheets', 'Cascading Simple Sheets'],
-//     correctAnswer: 2,
-// }
-
-// let questionFour = {
-//     question: "What year was JavaScript launched?",
-//     answers: ['1990', '1996', '1992', '1995'],
-//     correctAnswer: 3,
-// }
-
-// let questionFive = {
-//     question: "What does HTML stand for?",
-//     answers: ['Hypertext Markup Language', 'Hyperweb Markup Language', 'Hypertext Machine Language', 'Hyperpage Markdown Language'],
-//     correctAnswer: 0, 
-// }
+}
